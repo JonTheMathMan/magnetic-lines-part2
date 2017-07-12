90f36d6c1584a0538bd7a23d480a659f060b95f8
Jonathan Cherry <shadowfax@Jonathans-Mini.netgear.com>
2017-07-11 21:06:05 -0700

    built exact coodinate feature

--- js/buttons.js
+++ js/buttons.js
@@ -1,15 +1,67 @@
 function addSouths()
 {
-    poleTypeCreation.south = true;
-    poleTypeCreation.north = false;
-    document.getElementById("southButton").innerHTML = "Placing south poles.";
-    document.getElementById("northButton").innerHTML = "Place north poles.";
+    if(!poleTypeCreation.south){
+        poleTypeCreation.south = true;
+        poleTypeCreation.north = false;
+        document.getElementById("southButton").innerHTML = "Stop placing south poles.";
+        document.getElementById("northButton").innerHTML = "Place north poles.";
+    }else{
+        poleTypeCreation.south = false;
+        document.getElementById("southButton").innerHTML = "Place south poles.";
+    }
 }
 
 function addNorths()
 {
-    poleTypeCreation.north = true;
-    poleTypeCreation.south = false;
-    document.getElementById("northButton").innerHTML = "Placing north poles.";
-    document.getElementById("southButton").innerHTML = "Place south poles.";
+    if(!poleTypeCreation.north){
+        poleTypeCreation.north = true;
+        poleTypeCreation.south = false;
+        document.getElementById("northButton").innerHTML = "Stop placing north poles.";
+        document.getElementById("southButton").innerHTML = "Place south poles.";
+    }else{
+        poleTypeCreation.north = false;
+        document.getElementById("northButton").innerHTML = "Place north poles.";
+    }
+}
+
+function addExactSouth()
+{
+    var postionX = parseInt(document.getElementById("exactX").value);
+    var postionY = parseInt(document.getElementById("exactY").value);
+    
+    if(!isNaN(postionX) && !isNaN(postionY))
+        {
+            var southProperties = {
+                src:"./images/south.png",
+                pX:postionX,
+                pY:postionY,
+                poleType:"south"
+            }
+            souths.push(newMainPole(southProperties));
+        }else{
+            document.getElementById("exactX").value = "must be an integer";
+            document.getElementById("exactY").value = "must be an integer";
+        }
+    
+}
+
+function addExactNorth()
+{
+    var postionX = parseInt(document.getElementById("exactX").value);
+    var postionY = parseInt(document.getElementById("exactY").value);
+    
+    if(!isNaN(postionX) && !isNaN(postionY))
+        {
+            var northProperties = {
+                src:"./images/north.png",
+                pX:postionX,
+                pY:postionY,
+                poleType:"north"
+            }
+            norths.push(newMainPole(northProperties));
+        }else{
+            document.getElementById("exactX").value = "must be an integer";
+            document.getElementById("exactY").value = "must be an integer";
+        }
+    
 }
